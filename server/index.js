303a46deb55ece9e1c7dbd34664a671f32c8b215
Behzad Azampour <behy1368@gmail.com>
2016-11-18 01:58:14 +0000

    refactored code - includes mangodb

--- server/index.js
+++ server/index.js
@@ -12,65 +12,24 @@ const MONGODB_URI = "mongodb://localhost:27017/twtrData";
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// The in-memory database of tweets. It's a basic object with an array in it.
-// const db = require("./lib/in-memory-db");
-
-// The `data-helpers` module provides an interface to the database of tweets.
-// This simple interface layer has a big benefit: we could switch out the
-// actual database it uses and see little to no changes elsewhere in the code
-// (hint hint).
-//
-// Because it exports a function that expects the `db` as a parameter, we can
-// require it and pass the `db` parameter immediately:
-// const DataHelpers = require("./lib/data-helpers.js")(db);
-
-// The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
-// so it can define routes that use it to interact with the data layer.
-// const tweetsRoutes = require("./routes/tweets")(DataHelpers);
-
-// Mount the tweets routes at the "/tweets" path prefix:
-// app.use("/tweets", tweetsRoutes);
-
-
-
-
-
-
 MongoClient.connect(MONGODB_URI, (err, db) => {
+  // connect to the MongoDb.
   if (err) {
     console.error(`Failed to connect: ${MONGODB_URI}`);
     throw err;
   }
   console.log(`Connected to mongodb: ${MONGODB_URI}`);
 
+  // Read and prepare data from mongo using DataHelpers
   const DataHelpers = require("./lib/data-helpers.js")(db);
 
-  // // ==> Refactored and wrapped as new, tweet-specific function:
-  // function getTweets(callback) {
-  //   db.collection("tweets").find().toArray((err, tweets) => {
-  //     if (err) {
-  //       return callback(err);
-  //     }
-  //     callback(null, tweets);
-  //   });
-  // }
-
-
-  // // getTweets((err, tweets) => {
-  // //   if (err) throw err;
-  // //   console.log("Logging each tweet:");
-  // //   for (let tweet of tweets) {
-  // //     console.log(tweet['jsonTweets']);
-  // //   }
-
-  // //   db.close();
-  // // });
-
+  // define routes that use the DataHelper object.
   const tweetsRoutes = require("./routes/tweets")(DataHelpers);
+
+  // Mount the tweets routes at the "/tweets" path prefix:
   app.use("/tweets", tweetsRoutes);
 
 });
-
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -6,14 +6,15 @@ const simulateDelay = require("./util/simulate-delay");
 
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
-  db.collection('tweets').find().sort({"created_at":-1});
+
+// sort the data inside mongoDb
+db.collection('tweets').find().sort( { "created_at" : -1 } );
 
   return {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
       simulateDelay(() => {
-        //db.tweets.push(newTweet);
         db.collection('tweets').insert(newTweet);
         callback(null, true);
       });
@@ -24,31 +25,18 @@ module.exports = function makeDataHelpers(db) {
       simulateDelay(() => {
         function getTweets(callback) {
           db.collection("tweets").find().toArray((err, tweets) => {
-            if (err) {
-              return callback(err);
-            }
+            if (err) { return callback(err); }
             callback(null, tweets);
           });
         }
 
-
         getTweets((err, tweets) => {
-          console.log(tweets);
-          if (err) throw err;
+          if (err) { throw err; }
           callback(null, tweets);
-          //console.log("Logging each tweet:");
-          for (let tweet of tweets) {
-            //console.log(tweet);
-          }
-
-          //db.close();
         });
-
-
       });
-
-
     }
 
   };
 }
+//db.close();    <------------------------------------- Where do I put this?
